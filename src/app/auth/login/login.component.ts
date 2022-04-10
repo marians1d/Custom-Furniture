b2e28493f6b36579abc706bbf8f48b1042da05a5
Mariyan Damyanov <marians1md@gmail.com>
2022-04-10 23:14:49 +0300

    Login functionality update

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  loginHandler(form: NgForm): void {
+    if (form.invalid) { return; }
+    const { email, password } = form.value;
+  }
+
+}
