fc267af6cd8d4c1aa356cd1b551751251e045d45
Mariyan Damyanov <marians1md@gmail.com>
2022-04-11 23:12:46 +0300

    Inplemented header buttons base on user

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../auth/user.service';
+import { IUser } from 'src/app/shared/interfaces';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   isMenuCollapsed: boolean = true;
 
-  constructor() { }
+  
+  get isLogged() : boolean {
+    return this.userService.isLogged;
+  }
 
-  ngOnInit(): void {
+  get currentUser() : IUser | null | undefined {
+    return this.userService.user;
+  }
+  
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  logout(): void {
+    this.userService.logout$().subscribe(() => {
+      this.router.navigate(['/']);
+    });
   }
 
 }
