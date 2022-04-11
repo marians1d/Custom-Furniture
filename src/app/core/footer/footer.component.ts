89249d1c4f46014def9be81a0d4b23117b7bfa24
Mariyan Damyanov <marians1md@gmail.com>
2022-04-11 23:14:14 +0300

    Updating footer

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/auth/user.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
--- src/app/feature/pages/pages.module.ts
+++ src/app/feature/pages/pages.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NotFoundPageComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
