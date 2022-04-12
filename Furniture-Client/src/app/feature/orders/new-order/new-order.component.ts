668ca2a839664cf082835e2b9d5d6de779590b33
Mariyan Damyanov <marians1md@gmail.com>
2022-04-12 16:38:03 +0300

    Implementing new order functionality

--- Furniture-Client/src/app/feature/orders/new-order/new-order.component.ts
+++ Furniture-Client/src/app/feature/orders/new-order/new-order.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-new-order',
   templateUrl: './new-order.component.html',
   styleUrls: ['./new-order.component.css']
 })
-export class NewOrderComponent implements OnInit {
+export class NewOrderComponent {
 
-  constructor() { }
+  isPublic: boolean = true;
 
-  ngOnInit(): void {
-  }
+  initialStatus!: string;
+
+  constructor(private orderService: OrderService, private router: Router) {
+    this.initialStatus = 'public';
+   }
 
   submit(form: NgForm): void {
-    console.log(form.value);
+    if (form.invalid) { return; }
+    
+    this.orderService.createOrder$(form.value).subscribe({
+      next: (order) => {
+        console.log(order);
+
+        this.router.navigate([`/orders/${order._id}`]);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
   }
 }
