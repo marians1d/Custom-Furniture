05212fb7e5907af3050f63a35a179434750300ef
Mariyan Damyanov <marians1md@gmail.com>
2022-04-12 16:37:18 +0300

    adding order routing

--- Furniture-Client/src/app/feature/orders/order-routing.module.ts
+++ Furniture-Client/src/app/feature/orders/order-routing.module.ts
@@ -1,10 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NewOrderComponent } from './new-order/new-order.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 const routes: Routes = [
     // {
     //     path: 'orders'
     // },
+    {
+        path: 'orders/:orderId',
+        component: OrderDetailsComponent
+    },
     {
         path: 'orders/new',
         component: NewOrderComponent
