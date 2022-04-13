25f5ca285b5ee7f4067393a03dcb2e7da6aec31d
Mariyan Damyanov <marians1md@gmail.com>
2022-04-13 19:00:44 +0300

    Adding profile component

--- Furniture-Client/src/app/auth/profile/profile.component.ts
+++ Furniture-Client/src/app/auth/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  get user() {
+    return this.userService.user
+  }
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
