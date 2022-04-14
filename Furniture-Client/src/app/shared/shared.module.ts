d6e3db92ed90047a8381a79baffcc4f84e4d25ed
Mariyan Damyanov <marians1md@gmail.com>
2022-04-14 22:24:58 +0300

    Adding time difference pipe

--- Furniture-Client/src/app/shared/shared.module.ts
+++ Furniture-Client/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinerComponent } from './spiner/spiner.component';
+import { TimeDiffPipe } from './pipes/time-diff.pipe';
 
 
 
 @NgModule({
   declarations: [
-    SpinerComponent
+    SpinerComponent,
+    TimeDiffPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SpinerComponent
+    SpinerComponent,
+    TimeDiffPipe
   ]
 })
 export class SharedModule { }
