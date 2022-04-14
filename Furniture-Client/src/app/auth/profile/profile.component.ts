a952d46a2be99763856b3ff74d5239272c44ba26
Mariyan Damyanov <marians1md@gmail.com>
2022-04-14 22:21:17 +0300

    Updating profile component

--- Furniture-Client/src/app/auth/profile/profile.component.ts
+++ Furniture-Client/src/app/auth/profile/profile.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  isEditing: boolean = false;
 
   get user() {
-    return this.userService.user
+    return this.userService.user;
   }
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    
   }
 
+  submit(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+
+    this.userService.updateProfileInfo$(form.value).subscribe(() => {
+      this.isEditing = !this.isEditing;
+    });
+  }
+
+  getUser() {
+    console.log(this.user);
+    
+  }
 }
