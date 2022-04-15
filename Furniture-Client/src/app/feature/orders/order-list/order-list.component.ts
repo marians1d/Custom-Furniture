e81dcad40a2bb81c15ec75b07c9b9d2ce1ae3277
Mariyan Damyanov <marians1md@gmail.com>
2022-04-15 19:26:45 +0300

    Updating order list item

--- Furniture-Client/src/app/feature/orders/order-list/order-list.component.ts
+++ Furniture-Client/src/app/feature/orders/order-list/order-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
 import { IOrder } from 'src/app/shared/interfaces';
 import { OrderService } from '../../../core/services/order.service';
 
@@ -7,16 +9,23 @@ import { OrderService } from '../../../core/services/order.service';
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.css']
 })
-export class OrderListComponent implements OnInit {
+export class OrderListComponent implements OnInit, OnDestroy {
+
+  ordersSubscription?: Subscription;
 
   orderList!: IOrder[];
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private title: Title) { }
 
   ngOnInit(): void {
-    this.orderService.getOrders$().subscribe((orderList) => {
+    this.title.setTitle('Orders')
+
+    this.ordersSubscription = this.orderService.getOrders$().subscribe((orderList) => {
       this.orderList = orderList;
     });
   }
 
+  ngOnDestroy(): void {
+    this.ordersSubscription?.unsubscribe();
+  }
 }
