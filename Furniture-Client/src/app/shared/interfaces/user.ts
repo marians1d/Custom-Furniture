efd146b0bc5ea921e72f32789f255eb14b1c78cc
Mariyan Damyanov <marians1md@gmail.com>
2022-04-15 13:51:12 +0300

    small updates

--- Furniture-Client/src/app/shared/interfaces/user.ts
+++ Furniture-Client/src/app/shared/interfaces/user.ts
@@ -5,6 +5,7 @@ import { IOrder } from "./order";
 export interface IUser extends IBase {
   orders: IOrder[];
   comments: IComment[];
+  providing: IOrder[];
   status: string;
   email: string;
   username: string;
--- Furniture-Client/src/environments/environment.prod.ts
+++ Furniture-Client/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://git.heroku.com/custom-furniture.git/api'
 };
